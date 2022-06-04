666ae2462e16ecf411376e97796faa5e3905bc94
Alexey Malyukin <alexeymalyukin@gmail.com>
2022-06-04 20:52:24 +0300

    Record added, design improved
    Record added via local storage. Clear record button added as well. Small design changes with a title, move tracker.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const netPieces = document.getElementsByClassName('net_item');
 const winner = document.querySelector('.winner');
+const stat = document.querySelector('.stat');
 const restart = document.querySelector('.restart');
 const piece = document.getElementsByClassName('net_item');
+const clear = document.querySelector('.clear');
+
+const overlay = document.getElementById('overlay');
 
 let clickNum = 0;
 const tic = `X`;
@@ -10,16 +14,27 @@ const toe = `O`;
 var player = ''; 
 
 
+
+var o;
+localStorage.getItem('o') != null ? o=localStorage.getItem('o') : o = 0 ;
+
+var x;
+localStorage.getItem('x') != null ? x=localStorage.getItem('x') : x = 0 ;
+
+
+
 for (const piece of netPieces) {
+    winner.innerHTML = 'Xs moves';
     
     piece.addEventListener("click", function clickOn() {
          
         if (piece.contains == (tic || toe)) {
             piece.removeEventListener("click", clickOn);   
 
-        } else if (player == '' && clickNum >= 9) {
+        /*} else if (player == '' && clickNum >= 9) {
             piece.removeEventListener("click", clickOn);
-            winner.innerHTML = 'Its a draw!'; 
+            winner.innerHTML = `Its a draw! <p>Xs: ${x}</p>. <p>Os: ${o}.</p>`; 
+            //overlay.style.display = 'block'; */
 
         } else if (player == `Xs` || player == `Os`) {
             piece.removeEventListener("click", clickOn);
@@ -35,11 +50,17 @@ for (const piece of netPieces) {
                 winner.innerHTML = 'Os moves';
                 if (player == `Xs`) {
                     piece.removeEventListener("click", clickOn);
-                    winner.innerHTML = `Winner is ${player} `; 
+                    x++;
+                    winner.innerHTML = `Winner is ${player}!`;
+                    stat.innerHTML = `Statistics: <p>Xs: ${x}</p><p>Os: ${o}</p>`; 
+                    localStorage.setItem('x', x);
+                    //overlay.style.display = 'block';
                     
                 } else if (player == '' && clickNum >= 9) {
                     piece.removeEventListener("click", clickOn);
-                    winner.innerHTML = 'Its a draw!';
+                    winner.innerHTML = `It's a draw, try again!`
+                    stat.innerHTML = `Statistics: <p>Xs: ${x}</p><p>Os: ${o}</p>`;
+                    //overlay.style.display = 'block';
                 
                 }
     
@@ -50,12 +71,19 @@ for (const piece of netPieces) {
                 piece.removeEventListener("click", clickOn);
                 check();
                 winner.innerHTML = 'Xs moves';
+                
                 if (player == `Os`) {
                     piece.removeEventListener("click", clickOn);
-                    winner.innerHTML = `Winner is ${player} `; 
+                    o++;
+                    winner.innerHTML = `Winner is ${player}!`;
+                    stat.innerHTML = `Statistics: <p>Xs: ${x}</p><p>Os: ${o}</p>`;  
+                    localStorage.setItem('o', o);
+                    //overlay.style.display = 'block';
                 } else if (player == '' && clickNum >= 9) {
                     piece.removeEventListener("click", clickOn);
-                    winner.innerHTML = 'Its a draw!'; 
+                    winner.innerHTML = `It's a draw, try again!`
+                    stat.innerHTML = `Statistics: <p>Xs: ${x}</p><p>Os: ${o}</p>`;
+                    //overlay.style.display = 'block';
                 } 
             };
         };
@@ -77,19 +105,20 @@ const check = () => {
     ];
     for(i=0; i<arr.length; i++) {
         if(piece[arr[i][0]].innerHTML == 'X' && piece[arr[i][1]].innerHTML == 'X' && piece[arr[i][2]].innerHTML == 'X') {
-            player = `Xs`;
-            winner.innerHTML = `Winner is ${player} `; 
-            
+            player = `Xs`; 
 
         } else if (piece[arr[i][0]].innerHTML == 'O' && piece[arr[i][1]].innerHTML == 'O' && piece[arr[i][2]].innerHTML == 'O') {
-            player = `Os`;
-            winner.innerHTML = `Winner is ${player} `; 
-            
-            
+            player = `Os`;       
         } 
     }
 }
 
+
+clear.addEventListener("click", function(){
+    window.localStorage.removeItem('x');
+    window.localStorage.removeItem('o');
+    location.reload();
+})
 
    
 restart.addEventListener("click", function() {
